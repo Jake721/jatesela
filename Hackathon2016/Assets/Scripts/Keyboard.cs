72c261753871983cfac57b332bd70fe18014f215
SERENITY\jacob <jacob.bradley.ruth@gmail.com>
2016-11-19 15:50:48 -0500

    movement

---
+++ Hackathon2016/Assets/Scripts/Keyboard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Keyboard {
+
+    
+	public static bool isKeyDown(KeyCode key)
+    {
+        return Input.GetKey(key);
+    }
+}
---
+++ Hackathon2016/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Keyboard.isKeyDown(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-speed, 0));
+        }
+        if (Keyboard.isKeyDown(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(speed, 0));
+        }
+        if (Keyboard.isKeyDown(KeyCode.S))
+        {
+            rb.AddForce(new Vector2(0, -speed));
+        }
+        if (Keyboard.isKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, speed));
+        }
+    }
+}
